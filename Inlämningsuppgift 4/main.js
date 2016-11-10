b612a61b7e99e058960f573e2b3ccebcbc1a74c8
megnus <megnus@hotmail.com>
2016-11-10 18:32:47 +0100

    Almost done

--- Inlämningsuppgift 4/main.js
+++ Inlämningsuppgift 4/main.js
@@ -2,29 +2,99 @@
 * Inlämningsuppgift 3
 * Magnus Sundström
 * 2016-09-09
+https://developer.mozilla.org/en-US/docs/Web/Guide/HTML/Forms/Sending_forms_through_JavaScript
+https://developer.mozilla.org/en-US/docs/AJAX/Getting_Started
+http://mah-webbutv.github.io/courses/da281a_da265a/assignments/uppg4.html
 **/
 
 
 "use strict";
 
-// Objekt för att hantera AJAX
-var omdbAPI = new XMLHttpRequest();
-// Den URL vi ska använda oss av (obs. att denna har förvalt "the revenant")
-var omdbURL = "http://www.omdbapi.com/?t=the%20revenant&type=movie";
 
-// Vad vill vi göra när vi fått ett svar?
-omdbAPI.addEventListener("load", function() {
-    // Konvertera resultatet från JSON
-    var result = JSON.parse(this.responseText);
-    // Skriv ut resultatet
-    console.log(result);
+window.addEventListener("load", function () {
+	function sendData(movieTitle) {
+		/*  var XHR = new XMLHttpRequest();
+
+		// We bind the FormData object and the form element
+		var FD  = new FormData(form);
+
+		// We define what will happen if the data are successfully sent
+		XHR.addEventListener("load", function(event) {
+		  alert(event.target.responseText);
+		});
+
+		// We define what will happen in case of error
+		XHR.addEventListener("error", function(event) {
+		  alert('Oups! Something goes wrong.');
+		});
+
+		// We setup our request
+		XHR.open("POST", "http://ucommbieber.unl.edu/CORS/cors.php");
+
+		// The data sent are the one the user provide in the form
+		XHR.send(FD);*/
+
+		// Objekt för att hantera AJAX
+		var omdbAPI = new XMLHttpRequest();
+
+		// Den URL vi ska använda oss av (obs. att denna har förvalt "the revenant")
+		//var omdbURL = "http://www.omdbapi.com/?t=the%20revenant&type=movie";
+		var omdbURL = "http://www.omdbapi.com/?s=" + movieTitle + "&type=movie";
+
+		// Ange vilken metod (get) samt URL vi ska skicka med
+		omdbAPI.open("get", omdbURL, true);
+
+		// Skicka förfrågan
+		omdbAPI.send();
+
+		     // Vad vill vi göra när vi fått ett svar?
+		omdbAPI.addEventListener("load", function() {
+		    // Konvertera resultatet från JSON
+		    var result = JSON.parse(this.responseText);
+
+		    // Skriv ut resultatet
+		    console.log(result.Search);
+
+		    fetchMovies(result.Search)
+		});
+	}
+
+	function fetchMovies(searchResult) {
+		for (var index in searchResult) {
+	    	var title = searchResult[index].Title;
+	    	var year = searchResult[index].Year;
+			console.log(title, "(" + year + ")");
+	    }
+	}
+
+	// We need to access the form element
+	var form = document.getElementById("search-form");
+
+	// to takeover its submit event.
+	form.addEventListener("submit", function (event) {
+		event.preventDefault();
+		var val = form.elements["query"].value;
+		sendData(val);
+	});
 });
 
-// Ange vilken metod (get) samt URL vi ska skicka med
-omdbAPI.open("get", omdbURL, true);
-// Skicka förfrågan
-omdbAPI.send();
+// // Objekt för att hantera AJAX
+// var omdbAPI = new XMLHttpRequest();
+// // Den URL vi ska använda oss av (obs. att denna har förvalt "the revenant")
+// var omdbURL = "http://www.omdbapi.com/?t=the%20revenant&type=movie";
 
+// // Vad vill vi göra när vi fått ett svar?
+// omdbAPI.addEventListener("load", function() {
+//     // Konvertera resultatet från JSON
+//     var result = JSON.parse(this.responseText);
+//     // Skriv ut resultatet
+//     console.log(result);
+// });
+
+// // Ange vilken metod (get) samt URL vi ska skicka med
+// omdbAPI.open("get", omdbURL, true);
+// // Skicka förfrågan
+// omdbAPI.send();
 
 // // Uppgift 1
 // var messageBox = document.getElementById('message-box');
@@ -37,14 +107,15 @@ omdbAPI.send();
 // }
 
 
-// // Uppgift 2
+// Uppgift 2
 // var ul = document.getElementById("items");
-// document.getElementById('add-item').addEventListener('click', function() {
-// 	var text = prompt("Please create a item: ");
-// 	var li = document.createElement("li");
-// 	var textNode = document.createTextNode(text);
-// 	li.appendChild(textNode);
-// 	ul.appendChild(li);
+// document.getElementById('search-form_').addEventListener('click', function() {
+// 	// var text = prompt("Please create a item: ");
+// 	// var li = document.createElement("li");
+// 	// var textNode = document.createTextNode(text);
+// 	// li.appendChild(textNode);
+// 	// ul.appendChild(li);
+// 	console.log('hello');
 // });
 
 
@@ -74,9 +145,9 @@ omdbAPI.send();
 // 	// Del 1
 // 	var inputs = this.getElementsByTagName('input');
 // 	for (var i = 0; i < inputs.length; i++) {
-// 		console.log(inputs[i].name + " : " + inputs[i].value);	
+// 		console.log(inputs[i].name + " : " + inputs[i].value);
 // 	}
-	
+
 // 	// Del 2
 // 	var firstnameCharLenght = inputs.firstname.value.lenght;
 // 	var lastnameCharLenght = inputs.lastname.value.lenght;
@@ -105,6 +176,6 @@ omdbAPI.send();
 // 		event.target.submit();
 // 	}
 
-//     event.preventDefault();  
+//     event.preventDefault();
 // }
 // );
