a9fe5e0e1031f40380cfaa3f8af95f81ebcd56ef
megnus <megnus@hotmail.com>
2016-12-02 19:23:50 +0100

    Still working on assignment 5 with react

--- Inlämningsuppgift 5/Inl 5/main.js
+++ Inlämningsuppgift 5/Inl 5/main.js
@@ -23,7 +23,7 @@ setInterval(function() {
 	ReactDOM.render(
 		ExampleApplicationFactory({elapsed: new Date().getTime() - start}),
 		//React.createElement('h1', null, 'Hello!'),
-		document.getElementById('container')
+		document.getElementById('containerx')
 	);
 }, 50);
 
@@ -35,4 +35,93 @@ var MyComponent = React.createClass({
     }
 });
 
+
+var MyComponentx = React.createClass({
+    render: function() {
+        return (
+			<ul className="form-style-1">
+				<li>
+					<label>Full Name <span className="required">*</span></label>
+					<input type="text" name="field1" className="field-divided" placeholder="First" />&nbsp;
+					<input type="text" name="field2" className="field-divided" placeholder="Last" />
+				</li>
+				<li>
+					<label>Email <span className="required">*</span></label>
+					<input type="email" name="field3" className="field-long" />
+				</li>
+				<li>
+					<label>Subject</label>
+					<select name="field4" className="field-select">
+						<option value="Advertise">Advertise</option>
+						<option value="Partnership">Partnership</option>
+						<option value="General Question">General</option>
+					</select>
+				</li>
+				<li>
+					<label>Your Message <span className="required">*</span></label>
+					<textarea name="field5" id="field5" className="field-long field-textarea"></textarea>
+				</li>
+				<li>
+					<input type="submit" value="Submit" />
+				</li>
+			</ul>
+        );
+    }
+});
+
+ReactDOM.render(<MyComponentx/>, document.getElementById('container'));
 ReactDOM.render(<MyComponent/>, document.getElementById('test'));
+
+
+var FilteredList = React.createClass({
+  filterList: function(event){
+    var updatedList = this.state.initialItems;
+    updatedList = updatedList.filter(function(item){
+      return item.toLowerCase().search(
+        event.target.value.toLowerCase()) !== -1;
+    });
+    this.setState({items: updatedList});
+  },
+  getInitialState: function(){
+     return {
+       initialItems: [
+         "Apples",
+         "Broccoli",
+         "Chicken",
+         "Duck",
+         "Eggs",
+         "Fish",
+         "Granola",
+         "Hash Browns"
+       ],
+       items: []
+     }
+  },
+  componentWillMount: function(){
+    this.setState({items: this.state.initialItems})
+  },
+  render: function(){
+    return (
+      <div className="filter-list">
+        <input type="text" placeholder="Search" onChange={this.filterList}/>
+      <List items={this.state.items}/>
+      </div>
+    );
+  }
+});
+
+var List = React.createClass({
+  render: function(){
+    return (
+      <ul>
+      {
+        this.props.items.map(function(item) {
+          return <li key={item}>{item}</li>
+        })
+       }
+      </ul>
+    )
+  }
+});
+
+ReactDOM.render(<FilteredList/>, document.getElementById('mount-point'));
